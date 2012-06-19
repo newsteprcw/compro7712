7f936c13afcfff5a7f1c4a876d082274560ea217
newstep_rcw@hotmail.com <newstep_rcw@hotmail.com>
2012-06-19 14:40:25 +0700

    เพิ่มข้อความProgrammimg is fun

--- src/th/ac/nu/student/u54347712/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/student/u54347712/compro/ch01/HelloWorld.java
@@ -16,5 +16,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HelloWorld");
+        System.out.println("Programming is fun");
     }
 }
