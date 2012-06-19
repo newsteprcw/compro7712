b8e6234b13b4f0bd3a5dc5994a8254f7c53c7556
newstep_rcw@hotmail.com <newstep_rcw@hotmail.com>
2012-06-19 14:32:37 +0700

    สร้างHelloWorld

---
+++ src/th/ac/nu/student/u54347712/compro/ch01/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package th.ac.nu.student.u54347712.compro.ch01;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HelloWorld");
+    }
+}
